a3bee380d2802a74e4da0b82c2db59a44752fe23
Arafat Hossain <arafatctg58@yahoo.com>
2019-05-14 16:31:48 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function(){
+    $(window).scroll(function(){
+        var top=$(window).scrollTop();
+        if(top>=60){
+            $("nav").addClass('secondary');
+        }
+        else
+            if($("nav").hasClass('secondary')){
+                $('nav').removeClass('secondary');
+            }
+    });
+});
